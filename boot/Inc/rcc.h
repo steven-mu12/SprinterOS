cef2c6f38d19f4d0380c0fa2cbeef27d78edc09a
Steven Mu <xsmu0922@gmail.com>
2025-07-30 00:54:44 -0400

    SPRBOOT-12: Implement own styling guideline using Doxygen & 100 char limit

--- boot/Inc/rcc.h
+++ boot/Inc/rcc.h
@@ -27,7 +27,9 @@
 #include <stddef.h>
 #include "stm32f7.h"
 
-/* RCC structure */
+/**
+ * @brief RCC Structure
+ */
 struct rcc {
   volatile uint32_t CR, PLLCFGR, CFGR, CIR, AHB1RSTR, AHB2RSTR, AHB3RSTR,
       RESERVED0, APB1RSTR, APB2RSTR, RESERVED1[2], AHB1ENR, AHB2ENR, AHB3ENR,
